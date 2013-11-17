6dc2d5ea33bb253bb4505d2aa7b3f9ab680cb482
Eric Vince <eric@eaze-co.com>
2013-11-17 19:10:58 +0800

    "jQuery update"

--- homework/week5/jQuery - Test/main.js
+++ homework/week5/jQuery - Test/main.js
@@ -1,43 +1,46 @@
-// jQuery
+// jQuery = $
 
 
 // Shape Picker
 
 $("#buttonSquare").click(function() {
-	$("#bigSquare").css("visibility", "inherit");
+	$("#bigSquare")  .css("display", "inherit");
+	$("#bigCircle")  .css("display", "none"   );
+	$("#bigTriangle").css("display", "none"   );
 });
 
 $("#buttonCircle").click(function() {
-	$("#bigCircle").css("visibility", "inherit");
-	$("#bigSquare").css("float", "left");
+	$("#bigCircle")  .css("display", "inherit");
+	$("#bigSquare")  .css("display", "none"   );	
+	$("#bigTriangle").css("display", "none"   );
 });
 
 $("#buttonTriangle").click(function() {
-	$("#bigTriangle").css("visibility", "inherit");
-	$("#bigSquare").css("float", "left");
-	$("#bigCircle").css("float", "left");
+	$("#bigTriangle").css("display", "inherit"); 
+	$("#bigSquare")  .css("display", "none"   );
+	$("#bigCircle")  .css("display", "none"   );
 });
 
 
 // Color Picker
 
 $("#buttonBlue").click(function() {
-	$("#bigSquare").css("background-color", "#5c97e6")
-	$("#bigCircle").css("background-color", "#5c97e6")
+	$("#bigSquare")  .css("background-color", "#5c97e6")
+	$("#bigCircle")  .css("background-color", "#5c97e6")
 	$("#bigTriangle").css("border-bottom", "200px solid #5c97e6")
-	$("#a").css("color", "#5c97e6")
+	$("#a")          .css("color", "#5c97e6")
 });
 
 $("#buttonGreen").click(function() {
-	$("#bigSquare").css("background-color", "#98d9ad")
-	$("#bigCircle").css("background-color", "#98d9ad")
+	$("#bigSquare")  .css("background-color", "#98d9ad")
+	$("#bigCircle")  .css("background-color", "#98d9ad")
 	$("#bigTriangle").css("border-bottom", "200px solid #98d9ad")
-	$("#a").css("color", "#98d9ad")
+	$("#a")          .css("color", "#98d9ad")
 });
 
 $("#buttonRed").click(function() {
-	$("#bigSquare").css("background-color", "#ff998f")
-	$("#bigCircle").css("background-color", "#ff998f")
+	$("#bigSquare")  .css("background-color", "#ff998f")
+	$("#bigCircle")  .css("background-color", "#ff998f")
 	$("#bigTriangle").css("border-bottom", "200px solid #ff998f")
-	$("#a").css("color", "#ff998f")
+	$("#a")          .css("color", "#ff998f")
 });
