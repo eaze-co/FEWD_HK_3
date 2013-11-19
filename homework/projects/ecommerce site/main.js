66d1b4b400a5b6a921977643faef3e2e90e9d647
Eric Vince <eric@eaze-co.com>
2013-11-19 16:51:30 +0800

    "eCommerce update for Mart"

---
+++ homework/projects/ecommerce site/main.js
@@ -0,0 +1,92 @@
+
+// click picture
+
+$(this).click(function() {
+	$(this).toggle		
+
+}
+);
+
+
+// change picture
+
+
+// row1
+
+	// array1
+	var bandana1: = [
+	  "http://media2.hickorees.com/image/JSHomesteadIndigoDyedBandana_L1.jpg",
+	  "http://media2.hickorees.com/image/JSHomesteadIndigoDyedBandana_L2.jpg",
+	  "http://media2.hickorees.com/image/JSHomesteadIndigoDyedBandana_L3.jpg",
+	  "http://media2.hickorees.com/image/JSHomesteadIndigoDyedBandana_L4.jpg"
+	];
+	
+	// array2
+	var bandana2: = [
+	  "http://media2.hickorees.com/image/StanleySonsPatternBandanaBlack_L1.jpg",
+	  "http://media2.hickorees.com/image/StanleySonsPatternBandanaBlack_L2.jpg",
+	  "http://media2.hickorees.com/image/StanleySonsPatternBandanaBlack_L3.jpg",
+	  "http://media2.hickorees.com/image/StanleySonsPatternBandanaBlack_L4.jpg"
+	];
+	
+	// array3
+	var bandana3: = [
+	  "http://media2.hickorees.com/image/JSHomesteadBlackSulfurDyedBandana_L1.jpg",
+	  "http://media2.hickorees.com/image/JSHomesteadBlackSulfurDyedBandana_L2.jpg",
+	  "http://media2.hickorees.com/image/JSHomesteadBlackSulfurDyedBandana_L3.jpg",
+	  "http://media2.hickorees.com/image/JSHomesteadBlackSulfurDyedBandana_L4.jpg"
+	];
+	
+
+// row2
+
+	// array4
+	var bandana4: = [
+	  "http://media2.hickorees.com/image/StanleySonsBandanaOlive_L1.jpg",
+	  "http://media2.hickorees.com/image/StanleySonsBandanaOlive_L2.jpg",
+	  "http://media2.hickorees.com/image/StanleySonsBandanaOlive_L3.jpg",
+	  "http://media2.hickorees.com/image/StanleySonsBandanaOlive_L4.jpg"
+	];
+	
+	// array5
+	var bandana5: = [
+	  "http://media2.hickorees.com/image/StanleySonsBandanaNavy_L1.jpg",
+	  "http://media2.hickorees.com/image/StanleySonsBandanaNavy_L2.jpg",
+	  "http://media2.hickorees.com/image/StanleySonsBandanaNavy_L3.jpg",
+	  "http://media2.hickorees.com/image/StanleySonsBandanaNavy_L4.jpg"
+	];
+	
+	// array6
+	var bandana6: = [
+	  "http://media2.hickorees.com/image/StanleySonsBandanaCinnamon_L1.jpg",
+	  "http://media2.hickorees.com/image/StanleySonsBandanaCinnamon_L2.jpg",
+	  "http://media2.hickorees.com/image/StanleySonsBandanaCinnamon_L3.jpg",
+	  "http://media2.hickorees.com/image/StanleySonsBandanaCinnamon_L4.jpg"
+	];
+	
+
+// row3
+
+	// array7
+	var bandana7: = [
+	  "http://media2.hickorees.com/image/MadeontheMoonGizaBandanaBlack_L1.jpg",
+	  "http://media2.hickorees.com/image/MadeontheMoonGizaBandanaBlack_L2.jpg",
+	  "http://media2.hickorees.com/image/MadeontheMoonGizaBandanaBlack_L3.jpg",
+	  "http://media2.hickorees.com/image/MadeontheMoonGizaBandanaBlack_L4.jpg"
+	];
+	
+	// array8
+	var bandana8: = [
+	  "http://media2.hickorees.com/image/MadeontheMoonGizaBandanaRed_L1.jpg",
+	  "http://media2.hickorees.com/image/MadeontheMoonGizaBandanaRed_L2.jpg",
+	  "http://media2.hickorees.com/image/MadeontheMoonGizaBandanaRed_L3.jpg",
+	  "http://media2.hickorees.com/image/MadeontheMoonGizaBandanaRed_L4.jpg"
+	];
+	
+	// array9
+	var bandana9: = [
+	  "http://media2.hickorees.com/image/MadeontheMoonGizaBandanaNavy_L1.jpg",
+	  "http://media2.hickorees.com/image/MadeontheMoonGizaBandanaNavy_L2.jpg",
+	  "http://media2.hickorees.com/image/MadeontheMoonGizaBandanaNavy_L3.jpg",
+	  "http://media2.hickorees.com/image/MadeontheMoonGizaBandanaNavy_L4.jpg"
+	];
